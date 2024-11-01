f2bf253e72ab5b5607ed91bdc0c1700dc172f155
João Moreira <jgmoreira1809@gmail.com>
2024-11-01 16:38:44 -0300

    setting up eslint and architecture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
 import AppRoutes from './AppRoutes';
 
 export default function App() {
-  return <AppRoutes />
+  return <AppRoutes />;
 }
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+
 export default function Home() {
-    return (
-        <div>
-        <h1>Welcome to the Home Page</h1>
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <h1>Welcome to the Home Page</h1>
+    </>
+  );
 }
