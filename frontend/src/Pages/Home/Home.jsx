580ac3b097ebb8c027fdf1c78f1fc286fb335ac5
João Moreira <jgmoreira1809@gmail.com>
2024-11-01 16:41:32 -0300

    Deleted prettierrc

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Header from '../../Components/Header/Header';
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <h1>Welcome to the Home Page</h1>
+      <Footer />
     </>
   );
 }
