344ec71518c6fbc21f5546be8acade2723d3db38
João Moreira <jgmoreira1809@gmail.com>
2024-11-01 21:39:46 -0300

    Created Login poage

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Routers, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-
+import Home from "./pages/Home/Home";
 
 export default function AppRoutes() {
   return (
     <Routers>
       <Routes>
         <Route exact path="/" element={<Home />} />
-      </Routes> 
+      </Routes>
     </Routers>
   );
 }
